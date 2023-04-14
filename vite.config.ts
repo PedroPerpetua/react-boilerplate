311b5982775afc56eb388c0b1d74e18fcbeae3c5
PedroPerpetua <pedro.perpetua@sapo.pt>
2023-04-14 12:08:38 +0100

    Added typescript checking

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import checker from 'vite-plugin-checker';
 
 
 export default defineConfig({
-  plugins: [react(), eslint({ fix: true, lintOnStart: true })],
+  plugins: [
+    react(),
+    eslint({ fix: true, lintOnStart: true }),
+    checker({ typescript: true })],
 });
